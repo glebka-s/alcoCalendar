635a94d4a05d05b080094822a38a57134b1e00f6
glebka-s <startsev.gleb@gmail.com>
2026-02-24 22:42:40 +0300

    prepared all infrastructure

--- backend/AlcoCalendar.Api/Program.cs
+++ backend/AlcoCalendar.Api/Program.cs
@@ -1,16 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 // For containerized environment HTTP is usually enough; HTTPS is handled by reverse proxy if needed.
 // app.UseHttpsRedirection();
